eab19ff4ecd8ffa7ba678ac8f3235efb2883b54b
Aggeliki Gkavardina <56274254+aggelikigkavardina@users.noreply.github.com>
2026-02-14 23:23:50 +0200

    Implement add employee form and API integration

--- workforce-management-frontend/src/App.jsx
+++ workforce-management-frontend/src/App.jsx
@@ -1,12 +1,26 @@
 import './App.css'
+import EmployeeComponent from './components/EmployeeComponent'
+import FooterComponent from './components/FooterComponent'
+import HeaderComponent from './components/HeaderComponent'
 import ListEmployeeComponent from './components/ListEmployeeComponent'
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
 
   return (
     <>
-      <ListEmployeeComponent />
+    <BrowserRouter>
+      <HeaderComponent />
+        <Routes>
+          {/* // https://localhost:3030 */}
+          <Route path='/' element = { <ListEmployeeComponent /> }></Route>
+          {/* // https://localhost:3030/employees */}
+          <Route path='/employees' element = { <ListEmployeeComponent /> }></Route>
+          {/* // https://localhost:3030/add-employee */}
+          <Route path='/add-employee' element = { <EmployeeComponent /> } ></Route>
+        </Routes>
+      <FooterComponent />
+    </BrowserRouter>
     </>
   )
 }
